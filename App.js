925e6c4e42b58a027a0045e81bded4dd9f271e23
Benyamin <benyaminmahmoudyan@gmail.com>
2024-03-31 22:51:12 +0330

    test

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
+  const handlePress = () => (console.log("like handle press!"))
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress} >Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World!</Text>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +15,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50
   },
 });
---
+++ whatidid.md
@@ -0,0 +1,15 @@
+from React native course from Mosh
+`npm install expo`
+
+use `npx create-expo-app ProjectName` instead of `expo init ProjectName` 
+
+start app with `npm start`
+
+download and install android studio and jdk 
+https://developer.android.com/studio
+on ubuntu with `sudo apt install`
+
+after install android studio and test it I don't want to use it because my system can't handle it
+so i decide to uninstall it
+
+? search about expo publish
